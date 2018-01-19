99ccdb9a0aa98cbfd362b21f1d5bf022cb87fd72
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-01-20 00:29:34 +0800

    add the 1st problem, two sum

--- README.md
+++ README.md
@@ -4,5 +4,9 @@ This repository contains my solutions to some selected programming problems on [
 
 ## Table of Contents
 
+1. Two sum<br>
+[[Problem](https://leetcode.com/problems/two-sum/description/)] [[My Answer](src/leetcode/TwoSum.java)]
+
+
 ## Licence
 [GNU General Public Licence 3.0](LICENSE)
--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -1,4 +1,26 @@
 package leetcode;
 
+import java.util.HashMap;
+
 public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        // A mapping from the value to its index.
+        HashMap<Integer, Integer> mapping = new HashMap<>();
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            mapping.put(nums[i], i);
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            int other = target - nums[j];
+            if (mapping.containsKey(other) && mapping.get(other) != j) {
+                result[0] = j;
+                result[1] = mapping.get(other);
+                break;
+            }
+        }
+
+        return result;
+    }
 }
