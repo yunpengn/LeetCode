c806acbfe94e52e74b32ed4b5039b5c153f7fefc
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-06-22 13:59:05 +0800

    Try to fix three sums

--- src/main/java/leetcode/ThreeSum.java
+++ src/main/java/leetcode/ThreeSum.java
@@ -8,14 +8,6 @@ import java.util.Map;
 
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
-        // A mapping from all numbers to its respective index.
-        Map<Integer, Integer> numbers = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (!numbers.containsKey(nums[i])) {
-                numbers.put(nums[i], i);
-            }
-        }
-
         // Uses this to store the result.
         List<List<Integer>> result = new ArrayList<>();
 
@@ -24,13 +16,25 @@ public class ThreeSum {
             return result;
         }
 
+        // Sorting will not affect the overall time complexity since the lower bound should
+        // at least be about O(n^2).
+        Arrays.sort(nums);
+
+        // A mapping from all numbers to its respective index.
+        Map<Integer, Integer> numbers = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!numbers.containsKey(nums[i])) {
+                numbers.put(nums[i], i);
+            }
+        }
+
         // Iterates through the array.
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 int num1 = nums[i];
                 int num2 = nums[j];
                 int num3 = -num1 - num2;
-                if (numbers.containsKey(num3)) {
+                if (numbers.containsKey(num3) && numbers.get(num3) > j) {
                     result.add(Arrays.asList(num1, num2, num3));
                 }
             }
