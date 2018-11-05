e8599a46b30b300b088413ab091e0830ebe67bf6
Yunpeng Niu <neilniuyunpeng@gmail.com>
2018-11-06 01:34:36 +0800

    Add Lectures template

---
+++ src/main/java/others/Lectures.java
@@ -0,0 +1,11 @@
+package others;
+
+public class Lectures {
+    int calculateMinimumHalls(int N, int[] start, int[] end) {
+        return 0;
+    }
+
+    int calculateMinimumCancels(int N, int L, int[] start, int[] end) {
+        return 0;
+    }
+}
---
+++ src/test/java/others/LecturesTest.java
@@ -0,0 +1,17 @@
+package others;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class LecturesTest {
+    private static final Lectures tester = new Lectures();
+
+    @Test
+    void test1() {
+        int[] start = new int[]{ 3, 5, 4, 2, 1 };
+        int[] end = new int[]{ 4, 6, 7, 5, 5 };
+        assertEquals(3, tester.calculateMinimumHalls(start.length, start, end));
+        assertEquals(3, tester.calculateMinimumCancels(start.length, 2, start, end));
+    }
+}
